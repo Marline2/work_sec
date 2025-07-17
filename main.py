cc8c18cdc9d0ba734e1397a94e9abfd8a4e97b4b
Marline2 <marlinelee2@gmail.com>
2025-07-17 15:23:47 +0900

    cng name

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def get_top_sp500_companies():
     return response
 
 # 회사명 받아오면 company 코드 가져오기
-@app.get("/getCompanyFacts/{company_code}/{year}/", response_model=List[Company.CompanyFactRecord], status_code=status.HTTP_200_OK,
+@app.get("/getCompanyFacts/{sec_code}/{year}/", response_model=List[Company.CompanyFactRecord], status_code=status.HTTP_200_OK,
          responses={
         # 예외 상황을 Swagger UI에 명시
         400: {"description": "잘못된 요청 (Invalid Input)", "model": Response.ErrorResponseModel}, # 또는 에러 모델
@@ -34,12 +34,12 @@ def get_top_sp500_companies():
         500: {"description": "서버 내부 오류 (Internal Server Error)", "model": Response.ErrorResponseModel}
         # 필요하다면 다른 상태 코드도 추가 (예: 403 Forbidden, 401 Unauthorized 등)
     })
-def get_companyfacts(company_code: str, year: int):
+def get_companyfacts(sec_code: str, year: int):
     # SEC에서 데이터 가져오기
-    response = utils.get_companyfacts(company_code, year)
+    response = utils.get_companyfacts(sec_code, year)
     return response
 
-@app.get("/getFinData/{company_code}/", response_model=List[Company.YahooFinClosePrice], status_code=status.HTTP_200_OK,
+@app.get("/getFinData/{ticker_code}/", response_model=List[Company.YahooFinClosePrice], status_code=status.HTTP_200_OK,
          responses={
         # 예외 상황을 Swagger UI에 명시
         400: {"description": "잘못된 요청 (Invalid Input)", "model": Response.ErrorResponseModel}, # 또는 에러 모델
@@ -47,7 +47,7 @@ def get_companyfacts(company_code: str, year: int):
         500: {"description": "서버 내부 오류 (Internal Server Error)", "model": Response.ErrorResponseModel}
         # 필요하다면 다른 상태 코드도 추가 (예: 403 Forbidden, 401 Unauthorized 등)
     })
-def get_fin_data(company_code: str):
+def get_fin_data(ticker_code: str):
     # SEC에서 데이터 가져오기
-    response = utils.get_fin_data(company_code)
+    response = utils.get_fin_data(ticker_code)
     return response
